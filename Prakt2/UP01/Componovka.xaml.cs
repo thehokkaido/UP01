f074d939a21143f09b838353db5a2411dd53a4f1
Konstantin <133581000+thehokkaido@users.noreply.github.com>
2025-05-22 14:26:44 +0500

    Add files via upload

---
+++ Prakt2/UP01/Componovka.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UP01
+{
+    /// <summary>
+    /// Логика взаимодействия для Componovka.xaml
+    /// </summary>
+    public partial class Componovka : Window
+    {
+        public Componovka()
+        {
+            InitializeComponent();
+        }
+    }
+}
